ecf26d43e5cda6bbd12161946150fdcf01762e66
kramax42 <kramax42@yandex.by>
2023-01-16 16:33:58 +0300

    fix: move vertical animation logic to hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,3 +4,4 @@ export { useEventListener } from './event-listener.hook';
 export { useIsomorphicLayoutEffect } from './isomorphic-layout-effect.hook';
 export { usePortal } from './portal.hook';
 export { usePrevious } from './previous.hook';
+export { useVerticalAnimation } from './vertical-animation.hook';
---
+++ src/hooks/vertical-animation.hook.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+import { TOAST_VERICAL_TRANSFORM_DURATION } from '@/constants/animation';
+import { useIsomorphicLayoutEffect, usePrevious } from '@/hooks';
+import { calculateBoundingBoxes } from '@/utils/calculate-bounding-boxes';
+
+export const useVerticalAnimation = (children) => {
+  const [boundingBox, setBoundingBox] = useState({});
+  const [prevBoundingBox, setPrevBoundingBox] = useState({});
+  const prevChildren = usePrevious(children);
+
+  useIsomorphicLayoutEffect(() => {
+    const newBoundingBox = calculateBoundingBoxes(children);
+    setBoundingBox(newBoundingBox);
+  }, [children]);
+
+  useIsomorphicLayoutEffect(() => {
+    const prevBoundingBox = calculateBoundingBoxes(prevChildren);
+    setPrevBoundingBox(prevBoundingBox);
+  }, [prevChildren]);
+
+  useEffect(() => {
+    const hasPrevBoundingBox = Object.keys(prevBoundingBox).length;
+
+    if (hasPrevBoundingBox) {
+      React.Children.forEach(children, (child) => {
+        const domNode = child.ref.current;
+        const firstBox = prevBoundingBox[child.key];
+
+        const lastBox = boundingBox[child.key];
+        let changeInY = 0;
+        if (firstBox) {
+          changeInY = firstBox.top - lastBox.top;
+        }
+
+        if (changeInY) {
+          requestAnimationFrame(() => {
+            domNode.style.transform = `translateY(${changeInY}px)`;
+            domNode.style.transition = 'transform 0ms';
+
+            requestAnimationFrame(() => {
+              domNode.style.transform = '';
+              domNode.style.transition = `transform ${TOAST_VERICAL_TRANSFORM_DURATION}ms`;
+            });
+          });
+        }
+      });
+    }
+  }, [boundingBox, prevBoundingBox, children]);
+};
